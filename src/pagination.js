2e69d6868e901b9e4a5af4c1a62420494fb71dd6
bigmosi <kinyeramo@gmail.com>
2023-01-31 00:36:23 +0300

    added pagination pages

--- src/pagination.js
+++ src/pagination.js
@@ -3,4 +3,23 @@ export class Pagination {
     this.data = data;
     this.pageSize = pageSize;
   }
+
+  getPage(n) {
+    const offset = n * this.pageSize;
+
+    return this.data.slice(offset, offset + this.pageSize);
+  }
+
+  getTotalPages() {
+    return Math.ceil(this.data.length / this.pageSize);
+  }
+}
+
+export const PageNavigation = ({
+  nextPageHandler,
+  previousPageHandler,
+  currentPage,
+  totalPages,
+}) => {
+  
 }
